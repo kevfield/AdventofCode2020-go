d861046a3a757a8be0da1819818fceca0f842875
Kevin Field <kevfield@gmail.com>
2020-12-11 10:03:14 +0000

    day 8 p2 in progress but doesn't work yet

--- src/Day08/main.go
+++ src/Day08/main.go
@@ -2,44 +2,97 @@ package main
 
 import "fmt"
 
-func processOperations(pInput []string) {
+func processOperations(pInput []string) (int, bool) {
 
 	usedInstructions := make([]bool, len(pInput))
 	//startingPoint := usedInstructions[0]
 	currentPos := 0
-	accumulator := 0
+	p1accumulator := 0
+	var p2Found bool
 
 	var accAmount, jmpAmount int
 	var foundInstruction string
 
 	for usedInstructions[currentPos] == false {
-		fmt.Sscanf(pInput[currentPos], "%s", &foundInstruction)
+		if currentPos > len(pInput) {
+			// p2 found
+			p2Found = true
+			break
 
-		if foundInstruction == "acc" {
-			// get acc instruction
-			fmt.Sscanf(pInput[currentPos], "acc%d", &accAmount)
-			usedInstructions[currentPos] = true
-			accumulator = accumulator + accAmount
-			currentPos = currentPos + 1
+		} else {
+			fmt.Sscanf(pInput[currentPos], "%s", &foundInstruction)
 
-		} else if foundInstruction == "nop" {
-			usedInstructions[currentPos] = true
-			currentPos = currentPos + 1
+			if foundInstruction == "acc" {
+				// get acc instruction
+				fmt.Sscanf(pInput[currentPos], "acc%d", &accAmount)
+				usedInstructions[currentPos] = true
+				p1accumulator = p1accumulator + accAmount
+				currentPos = currentPos + 1
 
-		} else if foundInstruction == "jmp" {
-			fmt.Sscanf(pInput[currentPos], "jmp%d", &jmpAmount)
-			usedInstructions[currentPos] = true
-			currentPos = currentPos + jmpAmount
+			} else if foundInstruction == "nop" {
+				usedInstructions[currentPos] = true
+				currentPos = currentPos + 1
 
+			} else if foundInstruction == "jmp" {
+				fmt.Sscanf(pInput[currentPos], "jmp%d", &jmpAmount)
+				usedInstructions[currentPos] = true
+				currentPos = currentPos + jmpAmount
+
+			}
+
+			// reset variables
+			foundInstruction = ""
+			accAmount = 0
+			jmpAmount = 0
 		}
-
-		// reset variables
-		foundInstruction = ""
-		accAmount = 0
-		jmpAmount = 0
-
 	}
-	fmt.Println(accumulator)
+	return p1accumulator, p2Found
+}
+
+func changeInstructions(p2Input []string) int {
+	var returnfromP1 int
+	//p2currentPos := 0
+	var p2Result bool
+	changedInstructions := make([]bool, len(p2Input))
+	p2foundInstruction := ""
+	p2nopAmount := ""
+	p2jmpAmount := ""
+
+	for i, j := range p2Input {
+		if p2Result == false {
+			if changedInstructions[i] == false {
+				// find nop and jmp and change them
+				fmt.Sscanf(j, "%s", &p2foundInstruction)
+				if p2foundInstruction == "nop" {
+					// get amount
+					fmt.Sscanf(p2Input[i], "nop%s", &p2nopAmount)
+					p2Input[i] = "jmp " + p2nopAmount
+					changedInstructions[i] = true
+					returnfromP1, p2Result = processOperations(p2Input)
+
+				} else if p2foundInstruction == "jmp" {
+
+					// get amount
+					fmt.Sscanf(p2Input[i], "jmp%s", &p2jmpAmount)
+					p2Input[i] = "nop " + p2jmpAmount
+					changedInstructions[i] = true
+					returnfromP1, p2Result = processOperations(p2Input)
+				}
+
+			} else {
+				// instruction already changed, just process
+				returnfromP1, p2Result = processOperations(p2Input)
+			}
+		} else {
+			// p2 found
+			break
+		}
+		p2foundInstruction = ""
+		p2nopAmount = ""
+		p2jmpAmount = ""
+	}
+
+	return returnfromP1
 }
 
 func main() {
@@ -49,6 +102,12 @@ func main() {
 	// pull in puzzle input
 	puzzleInput, _ := readFile(inputFile)
 
-	processOperations(puzzleInput)
+	// Part 1
+	p1Answer, _ := processOperations(puzzleInput)
+	fmt.Println("Part 1: ", p1Answer)
+
+	// Part 2
+	p2Answer := changeInstructions(puzzleInput)
+	fmt.Println("Part 2: ", p2Answer)
 
 }
